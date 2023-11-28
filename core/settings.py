def27ea8924dc61c3ae241aba7733534ba1e59fa
ricardo <ricardoramospereira33@gmail.com>
2023-11-28 15:37:52 -0300

    config_02

--- core/settings.py
+++ core/settings.py
@@ -10,28 +10,53 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+from dotenv import load_dotenv
+from corsheaders.defaults import default_headers
 import os
+import sys
 
 # base_dir config
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# Adicionar essa tag para que nosso projeto encontre o .env
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+# Local onde estão os apps
+APPS_DIR = str(os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0, APPS_DIR)
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5z#hyg+dcm#)r)6sx6ka25(qbmo0=d4_g0^i5ntnt4ls*%1&x7'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    'X-Register',
+]
 
-# Application definition
+# CORS Config
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL como True, o que permite que qualquer site acesse seus recursos.
+# Defina como False e adicione o site no CORS_ORIGIN_WHITELIST onde somente o site da lista acesse os seus recursos.
+CORS_ALLOW_CREDENTIALS = False
+CORS_ORIGIN_WHITELIST = ['http://meusite.com',] # Lista.
 
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True # Redireciona http para https
+    ADMINS = [(os.getenv('SUPER_USER'), os.getenv('EMAIL'))] # Informações para o admin sobre erros
+    SESSION_COOKIE_SECURE = True # Cookies sejam definidos com conexões https
+    CSRF_COOKIE_SECURE = True # Proteje para usando HTTPS
+
+# Apps do django
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,14 +66,27 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+# Apps de terceiro
+THIRD_APPS = [
+    "corsheaders",
+]
+
+# Meu app
+PROJECT_APPS = [
+    'apps.base',
+    #'apps.myapp',
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware", # Cors
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'requestlogs.middleware.RequestLogsMiddleware', # Logs
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -79,9 +117,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
 'default': {
 'ENGINE': 'django.db.backends.sqlite3',
-'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'NAME': os.path.join(BASE_DIR, os.getenv('NAME_DB')),
+#'USER':os.getenv('USER_DB')
+#'PASSWORD': os.getenv('PASSWORD_DB')
+#'HOST':os.getenv('HOST_DB')
+#'PORT':os.getenv('PORT_DB')
 }
 }
+
+
 
 
 # Password validation
@@ -102,12 +146,40 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK={
+    'EXCEPTION_HANDLER': 'requestlogs.views.exception_handler',
+}
+
+# Logs
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'requestlogs_to_file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'info.log',
+        },
+    },
+    'loggers': {
+        'requestlogs': {
+            'handlers': ['requestlogs_to_file'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
+REQUESTLOGS = {
+    'SECRETS': ['password', 'token'],
+    'METHODS': ('PUT', 'PATCH', 'POST', 'DELETE'),
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 # Internationalization
-# Se quiser deixar em PT BR
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
@@ -134,3 +206,12 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Se tiver configuração de email
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
