c943ab6e47238b56b6cca197e82c56f9109f132a
ricardo <ricardoramospereira33@gmail.com>
2023-11-30 12:33:00 -0300

    register

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('timeout/', views.timeout_view, name='timeout')
+    path('timeout/', views.timeout_view, name='timeout'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,47 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from accounts.forms import CustomUserCreationForm
+from django.contrib.auth.models import Group, User
 
 # Create your views here.
 def timeout_view(request):
     return render(request, 'accounts/timeout.html')
+
+# Login
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Email ou senha inválidos')
+    if request.user.is_authenticated:
+        return redirect('home')
+    return render(request, 'accounts/login.html')
+
+# Registro
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save(commit=False)
+            usuario.is_valid = False
+            usuario.save()
+
+            group = Group.objects.get(name='usuario')
+            usuario.groups.add(group)
+
+            messages.success(request, 'Registrado. Agora faça o login para começar!')
+            return redirect('login')
+        else:
+            # Tratar quando usuario já existe, senhas... etc...
+            messages.error(request, 'A senha deve ter pelo menos 1 caractere maiúsculo, \
+    1 caractere especial e no minimo 8 caracteres.')
+    form = CustomUserCreationForm()
+    return render(request, "accounts/register.html",{"form": form})
