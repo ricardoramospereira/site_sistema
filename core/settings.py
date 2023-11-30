07dee91211deffc208c64329261a69e8f0040fcd
ricardo <ricardoramospereira33@gmail.com>
2023-11-30 10:41:55 -0300

    settings

--- core/settings.py
+++ core/settings.py
@@ -108,7 +108,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], #! Verificar o caminho
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
