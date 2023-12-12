7c8269b00384cae877fe15548017f3ee13498ead
ricardo <ricardoramospereira33@gmail.com>
2023-12-12 11:12:28 -0300

    detail_post

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -3,4 +3,7 @@ from posts import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('create-post', views.create_post, name='create_post'),
+    path('detail-post/<int:id>/', views.detail_post, name='detail-post')
+
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from config.utils import add_form_errors_to_messages
 from posts import models
+from django.shortcuts import get_object_or_404, render, redirect
+
+from posts.forms import PostagemForumForm
+from django.contrib import messages
 
 # Create your views here.
 def post_list(request):
@@ -8,3 +12,25 @@ def post_list(request):
         'posts': posts
     }
     return render(request, 'posts/list-post-forum.html', context=context)
+
+
+
+def create_post(request):
+    form = PostagemForumForm()
+    if request.method == 'POST':
+        form = PostagemForumForm(request.POST, request.FILES)
+        if form.is_valid():
+            forum = form.save(commit=False)
+            forum.usuario = request.user
+            form.save()
+            # Redirecionar para uma página de sucesso ou fazer qualquer outra ação desejada
+            messages.success(request, 'Seu Post foi cadastrado com sucesso!')
+            return redirect('post_list')
+        else:
+            add_form_errors_to_messages(request, form)
+    
+    return render(request, 'posts/form-post.html', {'form': form})
+
+def detail_post(request, id):
+    post = get_object_or_404(models.PostagemForum, id=id)
+    return render(request, 'posts/detail-post.html', {'post': post})
